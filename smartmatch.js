4d0c947f2adb7240b031f064612f55a948aafb8f
Jared Martin <jthomm@yahoo.com>
2012-07-12 01:54:58 -0400

    Break dependency on Underscore.js
    Adapted `_.isEqual` in order to break dependency on Underscore.

--- smartmatch.js
+++ smartmatch.js
@@ -1,34 +1,47 @@
 //     (c) 2012 J. Thomas Martin
 //     Smartmatch.js is freely distributable under the MIT license.
 //     Portions of Smartmatch.js are inspired or borrowed from Underscore.js, 
-//     as well as Perl's smartmatch operator.
+//     as well as Perl's smartmatch operator (http://perldoc.perl.org/perlop.html).
 
 (function() {
 
-  // Establish the root object, `window` in the browser, or `global` on the server.      
+  // Establish the root object (`window` in the browser, `global` on the server).
   var root = this;
 
-  var _ = root._;
-  // Require Underscore if we're on the server and it's not already present.
-  if (_ === void 0) {
-    if (require !== void 0) _ = require('underscore');
-    else throw new Error('Smartmatch.js requires Underscore.js');
-  }
-
-  // Create a reference for speed access to `toString`.
+  // Create references for easy access to `toString` and `hasOwnProperty`.
   var toString = Object.prototype.toString;
+  var hasOwn = Object.prototype.hasOwnProperty;
 
-  function isFalsey(obj) { return obj === null || obj === void 0 || obj === ''; }
-  function gte(x, y) { return isFalsey(y) || x >= y; }
-  function lt(x, y) { return isFalsey(y) || x < y; }
-  // Check whether object is within left-closed, right-open interval.
-  // Undefined, null, and empty string values unbound the interval.
+
+  // Functions used for interval comparison
+  // --------------------------------------
+
+  // Interval comparison interprets false-y values as unbounded.
+  function isUnbounded(obj) { return obj === null || obj === void 0 || obj === ''; }
+
+  // Intervals are left-closed.
+  function gte(x, y) { return isUnbounded(y) || x >= y; }
+
+  // Intervals are right-open.
+  function lt(x, y) { return isUnbounded(y) || x < y; }
+
+  // Check whether object is within left-closed, right-open, or unbounded interval.
   function isWithin(obj, array) { return gte(obj, array[0]) && lt(obj, array[1]); }
 
+
   // Check whether at least one item in `array` smartmatches with `obj`.
+  // -------------------------------------------------------------------
+
   function anyMatch(array, obj) {
-    return _(array).any(function(item) { return smartmatch(obj, item); });
+    for (var i=0, n=array.length; i<n; i++) {
+      if (smartmatch(obj, array[i])) return true;
+    }
+    return false;
   }
+
+
+  // When first argument to `smartmatch` is an array.
+  // ------------------------------------------------
 
   function arrayMatch(x, y) {
     switch(toString.call(y)) {
@@ -38,46 +51,141 @@
     }
   }
 
-  function functionMatch(x, y) {
-    return !!x(y);
+
+  // When first argument to `smartmatch` is a function.
+  // --------------------------------------------------
+
+  function functionMatch(x, y) { return !!x(y); }
+
+
+  // When first argument to `smartmatch` is a regular expression.
+  // ------------------------------------------------------------
+
+  function regExpEq(x, y) {
+    return x.source == y.source &&
+           x.global == y.global &&
+           x.multiline == y.multiline &&
+           x.ignoreCase == y.ignoreCase;
   }
 
   function regExpMatch(x, y) {
     switch(toString.call(y)) {
       case '[object Array]':    return anyMatch(y, x);
       case '[object Function]': return !!y(x);
-      case '[object RegExp]':   return _(x).isEqual(y);
+      case '[object RegExp]':   return regExpEq(x, y);
       default:                  return x.test(y);
     }
   }
+
+
+  // When first argument to `smartmatch` is a number.
+  // ------------------------------------------------
+
+  function numberEq(x, y) { return x != +x ? y != +y : (x == 0 ? 1/x == 1/y : x == +y); }
 
   function numberMatch(x, y) {
     switch(toString.call(y)) {
       case '[object Array]':    return y.length === 2 ? isWithin(x, y) : anyMatch(y, x);
       case '[object Function]': return !!y(x);
       case '[object RegExp]':   return y.test(x);
-      default:                  return _(x).isEqual(y);
+      case '[object Number]':   return numberEq(x, y);
+      default:                  return false;
     }
   }
+
+
+  // When first argument to `smartmatch` is a string.
+  // ------------------------------------------------
+
+  function stringEq(x, y) { return x == String(y); }
 
   function stringMatch(x, y) {
     switch(toString.call(y)) {
       case '[object Array]':    return anyMatch(y, x);
       case '[object Function]': return !!y(x);
       case '[object RegExp]':   return y.test(x);
-      default:                  return _(x).isEqual(y);
+      case '[object String]':   return stringEq(x, y);
+      default:                  return false;
     }
   }
+
+
+  // Deep comparison between objects and default equivalence handler.
+  // Abandons `smartmatch`-ing in favor of common sense equivalence.
+  // Closely adapted from Underscore.js' `_.isEqual` function.
+  // ----------------------------------------------------------------
+
+  function defaultEq(x, y, stack) {
+    if (x === y) return x !== 0 || 1/x == 1/y;
+    if (x == null || y == null) return x === y;
+    if (x._chain) a = x._wrapped;
+    if (y._chain) y = y._wrapped;
+    if (x.isEqual && typeof x.isEqual == 'function') return x.isEqual(y);
+    if (y.isEqual && typeof y.isEqual == 'function') return y.isEqual(x);
+    var className = toString.call(x);
+    if (className != toString.call(y)) return false;
+    switch (className) {
+      case '[object String]': return stringEq(x, y);
+      case '[object Number]': return numberEq(x, y);
+      case '[object Date]':
+      case '[object Boolean]': return +x == +y;
+      case '[object RegExp]': return regExpEq(x, y);
+    }
+    if (typeof x != 'object' || typeof y != 'object') return false;
+    var length = stack.length;
+    while (length--) {
+      if (stack[length] == x) return true;
+    }
+    stack.push(x);
+    var size = 0, result = true;
+    if (className == '[object Array]') {
+      size = x.length;
+      result = size == y.length;
+      if (result) {
+        while (size--) {
+          if (
+            !(result = size in x == size in y && defaultEq(x[size], y[size], stack))
+          ) break;
+        }
+      }
+    } else {
+      if (
+        'constructor' in x != 'constructor' in y || x.constructor != y.constructor
+      ) return false;
+      for (var key in x) {
+        if (hasOwn.call(x, key)) {
+          size++;
+          if (!(result = hasOwn.call(y, key) && defaultEq(x[key], y[key], stack))) break;
+        }
+      }
+      if (result) {
+        for (key in y) {
+          if (hasOwn.call(y, key) && !(size--)) break;
+        }
+        result = !size;
+      }
+    }
+    stack.pop();
+    return result;
+  }
+
+
+  // When first argument to `smartmatch` is not an array, 
+  // a function, a regular expression, a number, or a string.
+  // --------------------------------------------------------
 
   function defaultMatch(x, y) {
     switch(toString.call(y)) {
       case '[object Array]':    return anyMatch(y, x);
       case '[object Function]': return !!y(x);
-      default:                  return _(x).isEqual(y);
+      default:                  return defaultEq(x, y, []);
     }
   }
 
+
   // Core smartmatch function.
+  // -------------------------
+
   function smartmatch(x, y) {
     switch(toString.call(x)) {
       case '[object Array]':    return arrayMatch(x, y);
@@ -89,8 +197,9 @@
     }
   }
 
-  // Export `smartmatch`.  If we're in the browser, add `smartmatch` as
-  // a global object via a string identifier.
+
+  // Export `smartmatch`.  If we're in the browser, add 
+  // `smartmatch` as a global object via a string identifier.
   if (typeof exports !== 'undefined') {
     if (typeof module !== 'undefined' && module.exports) {
       exports = module.exports = smartmatch;
